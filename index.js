c4ef8d0aff949d9a41c944405771548587e1119e
Sam Flavin <flavi022@d.umn.edu>
2019-10-07 19:24:21 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log('Before');
+
+getUser(1, function(user){
+    console.log('User', user)
+
+    getRepos(user.gitHubUsername, function(repos){
+        console.log('Repos', repos)
+    })
+});
+
+console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        callback({ id: id, gitHubUsername: 'samflavin' })
+    }, 2000); 
+    }
+
+function getRepos (username, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        callback([{ username: username }, 'repo1', 'repo2', 'repo3'])
+    }, 2000);
+}
+    
